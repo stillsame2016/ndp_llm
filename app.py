ee323212c55c210b6c8afea3c62117c1323dcda4
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-05-22 10:56:10 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,53 +5,30 @@ import traceback
 import requests
 import streamlit as st
 
-import google.generativeai as genai
 from langchain.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+from langchain_groq import ChatGroq
 
-GOOGLE_API_KEY = st.secrets["GOOGLE_API_KEY"]
-genai.configure(api_key=GOOGLE_API_KEY)
-model = genai.GenerativeModel('gemini-pro')
+Groq_KEY = "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"
+Groq_KEY_2 = "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"
 
-safe = [
-    {
-        "category": "HARM_CATEGORY_HARASSMENT",
-        "threshold": "BLOCK_NONE",
-    },
-    {
-        "category": "HARM_CATEGORY_HATE_SPEECH",
-        "threshold": "BLOCK_NONE",
-    },
-    {
-        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-        "threshold": "BLOCK_NONE",
-    },
-    {
-        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-        "threshold": "BLOCK_NONE",
-    },
-]
-
-
-# Gemini uses 'model' for assistant; Streamlit uses 'assistant'
-def role_to_streamlit(role):
-    if role == "model":
-        return "assistant"
-    else:
-        return role
+llm = ChatGroq(temperature=0, model_name="llama3-70b-8192", api_key=Groq_KEY)
+llm2 = ChatGroq(temperature=0, model_name="llama3-70b-8192", api_key=Groq_KEY_2)
 
 
 # Add a Chat history object to Streamlit session state
 if "chat" not in st.session_state:
-    st.session_state.chat = model.start_chat(history=[])
+    st.session_state.chat = []
 
 # Display Form Title
 st.title("Chat with NDP LLM")
 
 # Display chat messages from history above current input box
-for message in st.session_state.chat.history:
-    pass  
-   
+for message in st.session_state.chat:
+    with st.chat_message(message['role']):
+        st.markdown(message['content'])
+st.write("")
+
 template = PromptTemplate(
     template="""You are the expert of Large Language Models on National Data Platform. 
 
@@ -62,47 +39,39 @@ template = PromptTemplate(
         Context:{context}
 
         Question:{question}?
-        
+
         Answer:""",
     input_variables=["question", "context"],
 )
+
+rag_chain = template | llm | StrOutputParser()
+rag_chain_2 = template | llm2 | StrOutputParser()
 
 # Accept user's next message, add to context, resubmit context to Gemini
 if prompt := st.chat_input("What can I help with?"):
 
     # Display user's last message
     st.chat_message("user").markdown(prompt)
+    st.session_state.chat.append({"role": "user", "content": prompt})
 
     with st.chat_message("assistant"):
-        with st.spinner("We are in the process of retrieving the relevant provisions to give you the best possible answer."):
+        with st.spinner(
+                "We are in the process of retrieving the relevant provisions to give you the best possible answer."):
             response = requests.get(f"https://sparcal.sdsc.edu/api/v1/Utility/llm?search_terms={prompt}")
             models = json.loads(response.text)
-            st.code(json.dumps(models, indent=4))
+            models = models[0:8]
 
             context = ""
             for model in models:
-                context += "\n\n"
-                context += f'author: {model["metadata"]["author"]}'
-                context += f'model id: {model["metadata"]["modelId"]}'
-                context += f'created_at: {model["metadata"]["created_at"]}'
-                context += f'downloads: {model["metadata"]["downloads"]}'
-                context += f'partial description: {model["document"]}'
-                
-            st.code(context)    
-                
-            
-            query = f"""
-                You are the expert of Large Language Models on National Data Platform. Based on the provided context, use easy 
-                understanding language to answer the question clear and precise with explanations. If no information is provided 
-                in the context, return the result as "Sorry I dont know the answer", don't provide the wrong answer or a 
-                contradictory answer.
+                context += "=======================================\n"
+                context += f'author: {model["metadata"]["author"]}\n'
+                context += f'model id: {model["metadata"]["modelId"]}\n'
+                context += f'created_at: {model["metadata"]["created_at"]}\n'
+                context += f'downloads: {model["metadata"]["downloads"]}\n'
+                context += f'partial description: {model["document"]}\n\n'
 
-                Context:{context}
+            result = rag_chain.invoke({"question": prompt, "context": context})
+            st.markdown(result)
+            st.session_state.chat.append({"role": "assistant", "content": result})
 
-                Question:{prompt}?
-        
-                Answer:""",
-            
-            response = st.session_state.chat.send_message(query, safety_settings=safe)
-            st.markdown(response.text)
             
