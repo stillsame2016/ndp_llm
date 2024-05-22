0bdfc29fdc4142988d156ab4dd36358fc4f2c6ee
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-05-22 11:43:25 -0700

    Update app.py

--- app.py
+++ app.py
@@ -31,9 +31,11 @@ st.write("")
 template = PromptTemplate(
     template="""You are the expert of Large Language Models on National Data Platform. 
 
-        Based on the provided context, use easy understanding language to answer the question clear and precise with 
-        explanations. If no information is provided in the context, return the result as "Sorry I dont know the answer", 
-        don't provide the wrong answer or a contradictory answer.
+        Based on the provided context, use easy understanding language to answer the question 
+        clear and precise with explanations. The context may contain truncated information; do 
+        not use truncated information in your answer.If no information is provided in the context, 
+        return the result as "Sorry I dont know the answer", don't provide the wrong answer 
+        or a contradictory answer. 
 
         Context:{context}
 
