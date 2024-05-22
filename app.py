69a096c93273de0485a9cd74fae0fc02c470cee3
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-05-22 12:00:17 -0700

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ if "chat" not in st.session_state:
     st.session_state.chat = []
 
 # Display Form Title
-st.title("Chat with NDP LLM")
+st.title("LLM Finder: Chat with NDP")
 
 # Display chat messages from history above current input box
 for message in st.session_state.chat:
