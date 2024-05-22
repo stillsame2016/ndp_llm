9c4c0bccbeb091678106a1379a9fb1c5ed18b7e1
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-05-22 10:03:32 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,62 @@
+import json
+import os
+import time
+import traceback
+
+import google.generativeai as genai
+import requests
+import streamlit as st
+from dotenv import load_dotenv
+
+GOOGLE_API_KEY = st.secrets["GOOGLE_API_KEY"]
+genai.configure(api_key=GOOGLE_API_KEY)
+model = genai.GenerativeModel('gemini-pro')
+
+safe = [
+    {
+        "category": "HARM_CATEGORY_HARASSMENT",
+        "threshold": "BLOCK_NONE",
+    },
+    {
+        "category": "HARM_CATEGORY_HATE_SPEECH",
+        "threshold": "BLOCK_NONE",
+    },
+    {
+        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        "threshold": "BLOCK_NONE",
+    },
+    {
+        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+        "threshold": "BLOCK_NONE",
+    },
+]
+
+
+# Gemini uses 'model' for assistant; Streamlit uses 'assistant'
+def role_to_streamlit(role):
+    if role == "model":
+        return "assistant"
+    else:
+        return role
+
+
+# Add a Chat history object to Streamlit session state
+if "chat" not in st.session_state:
+    st.session_state.chat = model.start_chat(history=[])
+
+# Display Form Title
+st.title("Chat with NDP LLM")
+
+# Display chat messages from history above current input box
+for message in st.session_state.chat.history:
+    pass  
+   
+
+# Accept user's next message, add to context, resubmit context to Gemini
+if prompt := st.chat_input("What can I help with?"):
+
+    # Display user's last message
+    st.chat_message("user").markdown(prompt)
+
+    with st.chat_message("assistant"):
+        st.markdown("OOPS")
