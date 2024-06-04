b6d2141f6a35cd22567cf81541c1075898abf0bc
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-06-04 08:50:59 -0700

    Update search_ndp_catalog.py

--- search_ndp_catalog.py
+++ search_ndp_catalog.py
@@ -61,19 +61,19 @@ def search_ndp_catalog(llm, user_input):
     response = requests.get(f"https://sparcal.sdsc.edu/staging-api/v1/Utility/ndp?search_terms={user_input}")
     datasets = json.loads(response.text)
 
-    result1 = question_planer.invoke({"question": user_input, "context": get_context(datasets[:5])})
-    result2 = question_planer.invoke({"question": user_input, "context": get_context(datasets[5:])})
+    # result1 = question_planer.invoke({"question": user_input, "context": get_context(datasets[:5])})
+    # result2 = question_planer.invoke({"question": user_input, "context": get_context(datasets[5:])})
     
-    # context = ""
-    # for dataset in datasets:    
-    #     title, description = dataset['description'].split("|", 1)
-    #     context += f"""
-    #                   Dataset Id: {dataset['dataset_id']}   
-    #                   Title: {title}            
-    #                   Description: {description} 
-    #                 """
-    #     if len(context) > 20000:
-    #         break
+    context = ""
+    for dataset in datasets:    
+        title, description = dataset['description'].split("|", 1)
+        context += f"""
+                      Dataset Id: {dataset['dataset_id']}   
+                      Title: {title}            
+                      Description: {description} 
+                    """
+        if len(context) > 20000:
+            break
     
-    # result = question_planer.invoke({"question": user_input, "context": context})
+    result = question_planer.invoke({"question": user_input, "context": context})
     return result1 + result2
